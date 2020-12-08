7b0beff75a8abbe731db365a5853caf3d7a63f4f
Linnea <32274390+LinneaXiaoXiang@users.noreply.github.com>
2020-12-07 19:40:42 -0800

    Fixes indentation and extra lines at end of file. References #1

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 # CS 361 Exam Practice
 
 class Thang
-
   def initialize(x, y, b)
     @width = x
     @height = y
@@ -10,25 +9,9 @@ class Thang
 
   # Calculate the volume (w x h x d)
   def volume
-        return @width * @height / @depth
+    return @width * @height / @depth
+  end
 end
-end
-
 
 cube = Thang.new(10, 20, 30)
 puts cube.volume
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
