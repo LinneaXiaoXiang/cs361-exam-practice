639cd101595340197b1e8ed55570da65390239a7
Linnea <32274390+LinneaXiaoXiang@users.noreply.github.com>
2020-12-07 19:48:30 -0800

    Improves class name from Thang to Cuboid. References #3

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # CS 361 Exam Practice
 
-class Thang
+class Cuboid
   def initialize(x, y, b)
     @width = x
     @height = y
@@ -13,5 +13,5 @@ class Thang
   end
 end
 
-cube = Thang.new(10, 20, 30)
+cube = Cuboid.new(10, 20, 30)
 puts cube.volume
